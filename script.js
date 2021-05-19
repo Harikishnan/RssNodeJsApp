b61a2810920f8fa9e6c62f7b9710e1702924a6f3
hari krishnan <harikrishnan9a@gmail.com>
2021-05-19 12:21:12 -0500

    all ready

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-pwa();
-
-
-function pwa(){
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(() => {
-    console.log("SW Registerd");
-  })
-};
+  navigator.serviceWorker
+      .register('/sw.js', { scope: '/' })
+      .then(() => {
+          console.info('Rss Tvndr App Is Registered');
+      }, err => console.error("Rss Tvndr App registration failed: ", err));
 
+  navigator.serviceWorker
+      .ready
+      .then(() => {
+          console.info('Rss Tvndr App Is Ready');
+      });
 }
-
-
 
 
 document.onreadystatechange = function () {
--- sw.js
+++ sw.js
@@ -1,47 +1,104 @@
 
-const toCache = [
-    '/'
+
+
+
+
+
+var CACHE_NAME = 'static-cache';
+var urlsToCache = [
+  '.',
+  '/404.html'
 ];
-
-
-
 self.addEventListener('install', function(event) {
-    console.log('Installed')
-    event.waitUntil(
-        caches.open('cache-name')
-          .then(function(cache) {
-             cache.addAll(toCache)
-          })
-          .then(self.skipWaiting())
-      )
-  })
-  
-  self.addEventListener('fetch', function(event) {
-    console.log("Fetched");
-    event.respondWith(
-        fetch(event.request)
-          .catch(() => {
-            return caches.open('cache-name')
-              .then((cache) => {
-                return cache.match(event.request)
-              })
-          })
-      )
-
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
     })
+  );
+});
+
+
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    return 404;
+  });
+}
+
+
+
+
+
+
+
+// const toCache = [
+//     '/',
+//     '404.html'
+// ];
+
+
+
+// self.addEventListener('install', function(event) {
+//     console.log('Installed')
+//     event.waitUntil(
+//         caches.open('cache-name')
+//           .then(function(cache) {
+//              cache.addAll(toCache)
+//           })
+//           .then(self.skipWaiting())
+//       )
+//   })
   
-  self.addEventListener('activate', function(event) {
-    console.log("Activated");  
-    event.waitUntil(
-      caches.keys()
-        .then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-            if (key !== 'cache-name') {
-              console.log('[ServiceWorker] Removing old cache', key)
-              return caches.delete(key)
-            }
-          }))
-        })
-        .then(() => self.clients.claim())
-    )
-})
+//   self.addEventListener('fetch', function(event) {
+//     console.log("Fetched");
+//     event.respondWith(
+//         fetch(event.request)
+//           .catch(() => {
+//             return caches.open('cache-name')
+//               .then((cache) => {
+//                 return cache.match(event.request)
+//               })
+//           })
+//       )
+
+//     })
+  
+//   self.addEventListener('activate', function(event) {
+//     console.log("Activated");  
+//     event.waitUntil(
+//       caches.keys()
+//         .then((keyList) => {
+//           return Promise.all(keyList.map((key) => {
+//             if (key !== 'cache-name') {
+//               console.log('[ServiceWorker] Removing old cache', key)
+//               return caches.delete(key)
+//             }
+//           }))
+//         })
+//         .then(() => self.clients.claim())
+//     )
+// })
